674ea27861ad5286c23defaebfcfc5488751ef6b
Abigail <ampatron@users.noreply.github.com>
2016-06-17 17:20:26 +0800

    Create PhoneCredit.java

---
+++ app/src/main/java/com/tdmr/tappdemo/models/PhoneCredit.java
@@ -0,0 +1,10 @@
+package com.tdmr.tappdemo.models;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * Created by Abigail on 6/17/2016.
+ */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
+public class PhoneCredit extends Product {
+}
