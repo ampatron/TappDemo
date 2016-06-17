cbf7315087dfe9396be976eca65417d21c6e8d81
Abigail <ampatron@users.noreply.github.com>
2016-06-17 17:21:17 +0800

    Added PhoneCredit subtype.

--- app/src/main/java/com/tdmr/tappdemo/models/CatalogItem.java
+++ app/src/main/java/com/tdmr/tappdemo/models/CatalogItem.java
@@ -1,6 +1,7 @@
 package com.tdmr.tappdemo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
@@ -11,9 +12,10 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonSubTypes(value = {
         @JsonSubTypes.Type(value = Category.class, name = "category"),
         @JsonSubTypes.Type(value = Product.class, name = "product"),
+        @JsonSubTypes.Type(value = PhoneCredit.class, name = "phone-credit"),
         @JsonSubTypes.Type(value = Service.class, name = "service")
 })
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public interface CatalogItem {
     @JsonIgnore
     String getTitle();
